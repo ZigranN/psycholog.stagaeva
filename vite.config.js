a3f6b5a442d2089f6a1236756df89b24b19af1c4
Nargiz <mam.zigran@gmail.com>
2024-12-16 12:11:43 +0600

    Подготовка к деплою 4

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Важно для правильной работы путей в продакшне
+  base: './', // Обеспечивает корректную работу путей в продакшене
   build: {
     outDir: 'dist',
   },
