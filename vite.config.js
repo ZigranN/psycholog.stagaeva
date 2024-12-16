c3370defc3b5ff9cb36ae1eb8626bb6a4dcd7779
Nargiz <mam.zigran@gmail.com>
2024-12-16 15:57:26 +0600

    Подготовка к деплою финальная

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: './', // Базовый путь для всех статических файлов
+  plugins: [react()], // Плагины, используемые в проекте
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html', // Входная точка для сборки
+      },
+    },
+    assetsInlineLimit: 0, // Предотвращает инлайнинг изображений в Base64
+    outDir: 'dist', // Каталог для сборки
+  },
+});
